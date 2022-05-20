f34f9ff5a946c837328361c6bf26ebf8d81255dc
tanmay <tan2018carlson@gmail.com>
2022-05-20 17:36:07 +0530

    update5

--- newpipeline.groovy
+++ newpipeline.groovy
@@ -27,7 +27,7 @@ pipeline{
   stage("Dev-Deployment"){
   steps{
       withCredentials([sshUserPrivateKey(credentialsId: 'gitnew', keyFileVariable: 'tomcat')]) {
-  sh 'ssh -i ${tomcat} ubuntu@3.7.68.98'
+  sh 'ssh -i ${tomcat}  -o StrictHostKeyChecking=no ubuntu@3.7.68.98'
   sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
   sh 'unzip awscliv2.zip'
   sh 'sudo ./aws/install'
